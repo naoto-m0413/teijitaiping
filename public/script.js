f2b680c73cd22d74f041d1536d0e0de1a90a6d75
Naoto Matsunaga <naoto.m.dev@gmail.com>
2026-03-20 15:26:41 +0900

    50%マイルストーンを「業務折り返し」として復活

--- public/script.js
+++ public/script.js
@@ -2058,6 +2058,11 @@ function updateStats() {
   el.progressFill.style.width  = `${pct}%`;
 
   // マイルストーン演出
+  if (!session.milestone50 && pct >= 50) {
+    session.milestone50 = true;
+    showMilestoneFlash("業務折り返し", "half");
+    playMilestoneSound("half");
+  }
   if (!session.milestone80 && pct >= 80) {
     session.milestone80 = true;
     showMilestoneFlash("もうすぐ退勤！", "near");
